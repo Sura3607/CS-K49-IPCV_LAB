eec35975834dfdbffdf2036ab9a42c2517423800
Nhựt Nguyễn <nguyenminhnhut3607@gmail.com>
2024-12-08 14:35:03 +0700

    10 update

--- Nhut/function.py
+++ Nhut/function.py
@@ -17,3 +17,11 @@ def multip_brightness(img,beta):
     b_img = img.astype(np.int16) * beta
     b_img = np.clip(b_img, 0, 255)
     return b_img.astype(np.uint8)
+
+def megre_to_frame(R0,G0,B0, A0):
+    frame = A0.copy()
+    frame[:600, :600] = cv2.addWeighted(R0, 100/255.0, frame[:600, :600], 1 - 100/255.0, 0)
+    frame[:600, 200:] = cv2.addWeighted(B0, 100/255.0, frame[:600, 200:], 1 - 100/255.0, 0)  
+    frame[200:, :600] = cv2.addWeighted(G0, 100/255.0, frame[200:, :600], 1 - 100/255.0, 0)
+    return cv2.cvtColor(frame, cv2.COLOR_RGBA2BGR)
+    
