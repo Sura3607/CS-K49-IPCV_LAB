6a86931f91b3f0a8a05707fbc7c96126ccb7f1dd
Nhựt Nguyễn <nguyenminhnhut3607@gmail.com>
2024-12-19 17:51:51 +0700

    ex9

--- Nhut/function.py
+++ Nhut/function.py
@@ -8,6 +8,8 @@ from urllib.request import urlretrieve
 
 from IPython.display import Image
 
+from collections import deque
+
 def brightness(img,alpha):
     b_img = img.astype(np.int16) + alpha 
     b_img = np.clip(b_img, 0, 255)
@@ -70,3 +72,32 @@ def binary_segmentation(img, threshold):
             if img[i,j] > threshold:
                 newImg[i,j] = 255
     return newImg
+
+def bfs(img,visited,i,j):
+    queue = deque([(i,j)])
+    visited[i][j] = True
+    pixel = []
+
+    while queue:
+        x, y = queue.popleft()
+        pixel.append((x,y))
+        for nx in range(x - 1, x + 2):  
+            for ny in range(y - 1, y + 2): 
+                if 0 <= nx < len(img) and 0 <= ny < len(img[0]):
+                    if not visited[nx][ny] and img[nx,ny] == 0:
+                        visited[nx][ny] = True
+                        queue.append((nx, ny))
+    return pixel                       
+
+def all_components(img):
+    rows, cols = img.shape
+    visited = [[False for _ in range(cols)] for _ in range(rows)]
+    components = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if not visited[i][j] and img[i,j] == 0:
+                bfs(img,visited,i,j)
+                components+=1
+
+    return components
